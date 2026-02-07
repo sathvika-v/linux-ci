797e0041e55d0ab87f24bc4d7e312aff8fed9370
Sathvika Vasireddy <sv@linux.ibm.com>
2026-02-07 11:13:25 +0000

    objtool: Fix "can't find starting instruction" warnings on vmlinux
    Objtool throws a lot of can't find starting instruction warnings
    when run on vmlinux with --ftr-fixup option.
    
    These warnings are seen because find_insn() function looks for
    instructions at offsets that are relative to the start of the section.
    In case of individual object files (.o), there are no can't find
    starting instruction warnings seen because the actual offset
    associated with an instruction is itself a relative offset since the
    sections start at offset 0x0.
    
    However, in case of vmlinux, find_insn() function fails to find
    instructions at the actual offset associated with an instruction
    since the sections in vmlinux do not start at offset 0x0. Due to
    this, find_insn() will look for absolute offset and not the relative
    offset. This is resulting in a lot of can't find starting instruction
    warnings when objtool is run on vmlinux.
    
    To fix this, pass offset that is relative to the start of the section
    to find_insn().
    
    find_insn() is also looking for symbols of size 0. But, objtool does
    not store empty STT_NOTYPE symbols in the rbtree. Due to this,
    for empty symbols, objtool is throwing can't find starting
    instruction warnings. Fix this by ignoring symbols that are of
    size 0 since objtool does not add them to the rbtree.
    
    Signed-off-by: Sathvika Vasireddy <sv@linux.ibm.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -412,7 +412,7 @@ static int decode_instructions(struct objtool_file *file)
 {
 	struct section *sec;
 	struct symbol *func;
-	unsigned long offset;
+	unsigned long offset, func_off;
 	struct instruction *insn;
 
 	for_each_sec(file->elf, sec) {
@@ -494,17 +494,25 @@ static int decode_instructions(struct objtool_file *file)
 			if (func->embedded_insn || func->alias != func)
 				continue;
 
-			if (!find_insn(file, sec, func->offset)) {
+			if (func->len == 0 && is_notype_sym(func))
+				continue;
+
+			func_off = opts.ftr_fixup ?
+				func->offset - sec->sh.sh_addr : func->offset;
+
+			if (!find_insn(file, sec, func_off)) {
 				ERROR("%s(): can't find starting instruction", func->name);
 				return -1;
 			}
 
-			sym_for_each_insn(file, func, insn) {
+			for (insn = find_insn(file, sec, func_off);
+				insn && insn->offset < func_off + func->len;
+				insn = next_insn_same_sec(file, insn)) {
 				insn->sym = func;
 				if (is_func_sym(func) &&
 				    insn->type == INSN_ENDBR &&
 				    list_empty(&insn->call_node)) {
-					if (insn->offset == func->offset) {
+					if (insn->offset == func_off) {
 						list_add_tail(&insn->call_node, &file->endbr_list);
 						file->nr_endbr++;
 					} else {
