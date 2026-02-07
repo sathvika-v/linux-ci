f721834f811b01c98bdba7c1aa1a762f9321c149
Sathvika Vasireddy <sv@linux.ibm.com>
2026-02-07 11:25:31 +0000

    objtool/powerpc: Skip jump destination analysis and unnanotated intra-function call warnings for --ftr-fixup
    Objtool is throwing unannotated intra-function call warnings
    when run on vmlinux with --ftr-fixup option.
    
    One such example:
    
    vmlinux: warning: objtool: .text+0x3d94:
                            unannotated intra-function call
    
    .text + 0x3d94 = c000000000008000 + 3d94 = c0000000000081d4
    
    c0000000000081d4: 45 24 02 48  bl c00000000002a618
    <system_reset_exception+0x8>
    
    c00000000002a610 <system_reset_exception>:
    c00000000002a610:       0e 01 4c 3c     addis   r2,r12,270
                            c00000000002a610: R_PPC64_REL16_HA    .TOC.
    c00000000002a614:       f0 6c 42 38     addi    r2,r2,27888
                            c00000000002a614: R_PPC64_REL16_LO    .TOC.+0x4
    c00000000002a618:       a6 02 08 7c     mflr    r0
    
    This is happening because we should be looking for destination
    symbols that are at absolute offsets instead of relative offsets.
    After fixing dest_off to point to absolute offset, there are still
    a lot of these warnings shown.
    
    In the above example, objtool is computing the destination
    offset to be c00000000002a618, which points to a completely
    different instruction. find_call_destination() is looking for this
    offset and failing. Instead, we should be looking for destination
    offset c00000000002a610 which points to system_reset_exception
    function.
    
    Even after fixing the way destination offset is computed, and
    after looking for dest_off - 0x8 in cases where the original offset
    is not found, there are still a lot of unannotated intra-function
    call warnings generated. This is due to symbols that are not
    properly annotated.
    
    So, for now, as a hack to curb these warnings, do not emit
    unannotated intra-function call warnings when objtool is run
    with --ftr-fixup option.
    
    Skip add_jump_destinations() and suppress intra-function call
    errors in --ftr-fixup mode. The feature fixup pass only needs fixup
    entry tables, relocation entries from .__ftr_alternates.text, and
    elf_write_insn() to patch branch offsets.
    
    Signed-off-by: Sathvika Vasireddy <sv@linux.ibm.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -1603,7 +1603,6 @@ static int add_jump_destinations(struct objtool_file *file)
 			    dest_off == func->offset + func->len)
 				continue;
 
-
 			ERROR_INSN(insn, "can't find jump dest instruction at %s",
 				offstr(dest_sec, dest_off));
 			return -1;
@@ -1686,8 +1685,11 @@ static int add_call_destinations(struct objtool_file *file)
 				continue;
 
 			if (!insn_call_dest(insn)) {
-				ERROR_INSN(insn, "unannotated intra-function call");
-				return -1;
+				if (!opts.ftr_fixup) {
+					ERROR_INSN(insn, "unannotated intra-function call");
+					return -1;
+				}
+				continue;
 			}
 
 			if (func && !is_func_sym(insn_call_dest(insn))) {
@@ -2641,8 +2643,10 @@ static int decode_sections(struct objtool_file *file)
 			return -1;
 	}
 
-	if (add_jump_destinations(file))
-		return -1;
+	if (!opts.ftr_fixup) {
+		if (add_jump_destinations(file))
+			return -1;
+	}
 
 	/*
 	 * Must be before add_call_destination(); it changes INSN_CALL to
